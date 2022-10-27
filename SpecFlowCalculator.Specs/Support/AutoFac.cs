a73df7714c105c282096df9446c31eaeed5b430c
Björn Weström <Bjorn.Westrom@who-umc.org>
2022-10-27 15:39:13 +0200

    initial

---
+++ SpecFlowCalculator.Specs/Support/AutoFac.cs
@@ -0,0 +1,31 @@
+﻿
+using Autofac;
+using SpecFlow.Autofac;
+using SpecFlow.Autofac.SpecFlowPlugin;
+using SpecFlowCalculator.Specs.API;
+using SpecFlowCalculator.Specs.Steps;
+using System;
+
+namespace SpecFlowCalculator.Specs.Support
+{
+    public static class AutoFacInit
+    {
+          
+        [GlobalDependencies]
+        public static void CreateGlobalContainer(ContainerBuilder containerBuilder)
+        {
+            Console.WriteLine("CreateGlobalContainer");
+            containerBuilder.RegisterType<CalculatorApi>();
+        }
+          
+        
+        [ScenarioDependencies]
+        public static void CreateContainerBuilder(ContainerBuilder containerBuilder)
+        {
+            // Register scenario scoped runtime dependencies
+            Console.WriteLine("CreateContainerBuilder");
+
+            containerBuilder.AddSpecFlowBindings<CalculatorStepDefinitions>();
+        }
+    }
+}
